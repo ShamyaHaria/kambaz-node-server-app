22493825e6fc26abe123445a6b90124251bfd2bf
Shamya <shamyaharia@gmail.com>
2025-11-14 00:14:11 -0500

    CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-const app = express()
+import cors from "cors";
+const app = express();
+app.use(cors());
 Hello(app);
 Lab5(app);
 
